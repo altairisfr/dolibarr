33558945b1c13abed13c2f56aa4795771e1a3a7c
Mélina JOUM <melina.joum@altairis.fr>
2025-06-10 18:17:32 +0200

    ADD: preg_quote to include internal and external PDFs for PDF merge

--- htdocs/core/actions_massactions.inc.php
+++ htdocs/core/actions_massactions.inc.php
@@ -841,7 +841,7 @@ if (!$error && $massaction == "builddoc" && $permissiontoread && !GETPOST('butto
 
 	$arrayofinclusion = array();
 	foreach ($listofobjectref as $tmppdf) {
-		$arrayofinclusion[] = '^'.preg_quote(dol_sanitizeFileName($tmppdf), '/').'\.pdf$';
+		$arrayofinclusion[] = preg_quote(dol_sanitizeFileName($tmppdf), '/').'\.pdf$';
 	}
 	foreach ($listofobjectref as $tmppdf) {
 		$arrayofinclusion[] = '^'.preg_quote(dol_sanitizeFileName($tmppdf), '/').'_[a-zA-Z0-9\-\_\'\&\.]+\.pdf$'; // To include PDF generated from ODX files
